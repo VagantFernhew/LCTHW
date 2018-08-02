b8957d0a2b66020aec94b70e9b425dc62a69be46
Sher Azam Hussain <azamhussain.25@iiee.edu.pk>
2018-08-02 12:16:35 -0600

    first c progaram

---
+++ ex1/ex1.c
@@ -0,0 +1,20 @@
+/**
+ * @file ex1
+ * @brief a simple c program
+ *
+ * @dated 02-08-2018
+ *
+ **/
+
+#include <stdio.h>
+
+/*This is a comment. */
+int main(int argc, char const *argv[])
+{
+	int distance = 100;
+
+	// this is also a comment
+	printf("You are %d miles away.\n", distance );
+	printf("Congrats.! you just build your program using makefile.\n");
+	return 0;
+}
