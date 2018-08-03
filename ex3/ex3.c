83176b4ccbb5e4c2580ee65ac50d7418d6bb88c4
Sher Azam Hussain <azamhussain.25@iiee.edu.pk>
2018-08-03 12:54:36 -0600

    formating output

---
+++ ex3/ex3.c
@@ -0,0 +1,20 @@
+/**
+* @file ex3.c
+* @brief formating output
+*
+* @date Agust 03, 2018
+*
+**/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int age = 5;
+	int height = 72;
+
+	printf("I am %d years old.\n", age );
+	printf("I am %d inches tall.\n", height );
+
+	return 0;
+}
