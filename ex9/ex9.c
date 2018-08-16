5a0609dddc2ec44973627a7e8050478ae275e257
Sher Azam Hussain <azamhussain.25@iiee.edu.pk>
2018-08-16 12:19:33 -0600

    while-loop and boolean experession

---
+++ ex9/ex9.c
@@ -0,0 +1,20 @@
+/**
+ * @file ex9.c
+ * @brief while loop and boolean experession
+ *
+ * @date 09-08-2018 
+ **/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int i = 0;
+	while( i < 25)
+	{
+		printf("%d,  ", i );
+		i++;
+	}
+	printf("\n");
+	return 0;
+}
